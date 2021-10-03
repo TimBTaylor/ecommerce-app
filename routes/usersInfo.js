8cf0a7c30884c71c50ca1d7b4a1716fd1a12ce7c
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2021-10-02 22:50:15 -0400

    user update request

--- routes/usersInfo.js
+++ routes/usersInfo.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcrypt");
 const getUser = require("../middleware/getUser");
 
 //updated user
-router.post("/:id/update-user", authorize, getUser, async (req, res) => {
+router.post("/:id/update-user", getUser, async (req, res) => {
   try {
     bcrypt.hash(req.body.password, 10).then((hashed) => {
       res.user.firstName = req.body.firstName;
